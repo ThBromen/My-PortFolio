529377798d49f52f07adbe13fe89a96d7bc743bd
ThBromen <hashimwimanatheogene34@gmail.com>
2023-09-01 09:28:58 +0200

    adding responsiveness

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+let toggleButton = document.getElementById('button')
 
+
+let navBarLinks = document.getElementById('links')
+
+
+toggleButton.addEventListener("click",()=>{
+     navBarLinks.classList.toggle('active')
+})
+ 
